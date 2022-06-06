f7b6c6e4274ab7d500db25571145c83339611d07
agral <adam@gralin.ski>
2022-06-06 21:04:03 +0200

    Add a GUI window with a status bar
    Implements the main GUI window for Fakturant.
    The window is empty, but it has a status bar with a greeting.
    
    The window handles close signal correctly.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 
+#include <wx/wx.h>
+
 #include "Util.hpp"
 
-int main() {
-  std::cout << "Util: " << Util::foo(42) << "\n";
+class GuiFakturantApp: public wxApp
+{
+ public:
+  virtual bool OnInit();
+};
+
+class GuiFrameMain: public wxFrame
+{
+ public:
+  GuiFrameMain(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+ private:
+  void OnExit(wxCommandEvent& event);
+};
+
+bool GuiFakturantApp::OnInit()
+{
+  GuiFrameMain* frame = new GuiFrameMain("Fakturant", wxPoint(50, 50), wxSize(1200, 960));
+  frame->Show(true);
+  return true;
 }
+
+GuiFrameMain::GuiFrameMain(const wxString& title, const wxPoint& pos, const wxSize& size)
+: wxFrame(nullptr, wxID_ANY, title, pos, size)
+{
+  CreateStatusBar();
+  SetStatusText("Witaj w programie Fakturant");
+}
+
+void GuiFrameMain::OnExit(wxCommandEvent& event)
+{
+  Close(true);
+}
+
+// a macro that provides a main() method:
+wxIMPLEMENT_APP(GuiFakturantApp);
