6af92fc518fe69c56fe64f2399b46db0f396d751
agral <adam@gralin.ski>
2022-06-08 21:55:20 +0200

    Use Application class instead of inplace code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
-#include <iostream>
-
 #include <wx/wx.h>
+#include "gui/Application.hpp"
 
-#include "gui/frame/Main.hpp"
-
-class GuiFakturantApp: public wxApp
-{
- public:
-  virtual bool OnInit();
-};
-
-bool GuiFakturantApp::OnInit()
-{
-  gui::frame::Main* frame = new gui::frame::Main("Fakturant", wxPoint(50, 50), wxSize(1200, 960));
-  frame->Show(true);
-  return true;
-}
-
-// a macro that provides a main() method:
-wxIMPLEMENT_APP(GuiFakturantApp);
+// a wxWidgets macro that provides main() method:
+wxIMPLEMENT_APP(gui::Application);
